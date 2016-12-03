8fd15a91116dac579cda8d112dafa5f4d3b57f0a
Seth Etter <sethetter@gmail.com>
2016-12-03 17:04:16 -0600

    Put Main.hs back to normal

--- aoc-day01/app/Main.hs
+++ aoc-day01/app/Main.hs
@@ -4,10 +4,7 @@ import AocDay01
 import Data.List.Split
 
 main :: IO ()
-main = 
-  {-inputStr <- readFile "input.txt"-}
-  {-let finalStatus = navigateFromStr "R8"-}
-  {-let finalStatus = navigateFromStr "R8, R4"-}
-  let finalStatus = last $ navigateFromStr "R8, R4, R4"
-  {-let finalStatus = navigateFromStr "R8, R4, R4, R8"-}
+main = do
+  inputStr <- readFile "input.txt"
+  let finalStatus = last $ navigateFromStr inputStr
    in putStr $ showStatus finalStatus ++ showDistanceFromZero finalStatus
