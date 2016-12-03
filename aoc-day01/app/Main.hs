b60b812538a932210374a3d347862672a28bf8bd
Seth Etter <sethetter@gmail.com>
2016-12-03 09:28:21 -0600

    Day 1! Except the answer is wrong.. hmm.

--- aoc-day01/app/Main.hs
+++ aoc-day01/app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Lib
+import AocDay01
+import Data.List.Split
 
 main :: IO ()
-main = someFunc
+main = do
+  -- read in the file contents as a string
+  inputString <- readFile "input.txt"
+  let instructionStrings = splitOn ", " inputString
+      instructions       = map parseInstruction instructionStrings
+      finalStatus        = navigate instructions
+   in putStr $ showStatus finalStatus ++ showDistanceFromZero finalStatus
---
+++ aoc-day01/src/AocDay01.hs
@@ -0,0 +1,57 @@
+module AocDay01
+  ( navigate
+  , parseInstruction
+  , showStatus
+  , showDistanceFromZero
+  ) where 
+
+data Direction = North | South | East | West
+  deriving (Show)
+
+data Turn = L | R
+  deriving (Show)
+
+data Instruction
+  = Instruction Turn Integer
+  | Unknown
+  deriving (Show)
+
+type Coordinates = (Integer, Integer)
+
+type Status = (Direction, Coordinates)
+
+
+navigate :: [Instruction] -> Status
+navigate = foldr move (North, (0, 0))
+
+move :: Instruction -> Status -> Status
+move (Instruction L blocks) (facing, (x, y)) =
+  case facing of
+    North -> (West,  (x - blocks, y))
+    East  -> (North, (x, y + blocks))
+    South -> (East,  (x + blocks, y))
+    West  -> (South, (x, y - blocks))
+move (Instruction R blocks) (facing, (x, y)) =
+  case facing of
+    North -> (East,  (x + blocks, y))
+    East  -> (South, (x, y - blocks))
+    South -> (West,  (x - blocks, y))
+    West  -> (North, (x, y + blocks))
+
+-- Takes something like "R13" or "L191"
+parseInstruction :: String -> Instruction
+parseInstruction string =
+  case take 1 string of
+    "R" -> Instruction R blocks
+    "L" -> Instruction L blocks
+    _   -> Unknown
+  where blocks = read $ tail string :: Integer
+
+showStatus :: Status -> String
+showStatus (facing, (x, y)) =
+  "(" ++ show x ++ ", " ++ show y ++ "), Facing: " ++ show facing ++ "\n"
+
+showDistanceFromZero :: Status -> String
+showDistanceFromZero (_, (x, y)) =
+  let distance = abs x + abs y
+   in "Blocks away: " ++ show distance ++ "\n"
