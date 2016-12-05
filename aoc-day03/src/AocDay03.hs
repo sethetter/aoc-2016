325c54b24d88115b233cc436d02bb90d689a7135
Seth Etter <sethetter@gmail.com>
2016-12-05 15:30:51 -0600

    Day 3 part 2 done!

--- aoc-day03/src/AocDay03.hs
+++ aoc-day03/src/AocDay03.hs
@@ -1,17 +1,35 @@
 module AocDay03
   ( validTriangle
   , lineToSides
+  , sideListToColumnGroups
   ) where
 
+import Data.List.Split
+import Data.List        (foldl')
 
-type Sides = (Integer, Integer, Integer)
+
+type Sides = [Integer]
 
 
 validTriangle :: Sides -> Bool
-validTriangle (a, b, c) = (a + b) > c && (a + c) > b && (b + c) > a
+validTriangle sides =
+  let a = head sides; b = head $ tail sides; c = last sides
+   in (a + b) > c && (a + c) > b && (b + c) > a
 
 
 lineToSides :: String -> Sides
 lineToSides line =
   let sidesList = (read :: String -> Integer) <$> words line
-   in (head sidesList, head (tail sidesList), last sidesList)
+   in [head sidesList, head (tail sidesList), last sidesList]
+
+
+sideListToColumnGroups :: [Sides] -> [Sides]
+sideListToColumnGroups sides =
+  let groupsOfThree = chunksOf 3 sides
+   in foldl' (\acc group -> acc ++ threeSidesToColumnGroups group) [] groupsOfThree
+
+
+threeSidesToColumnGroups :: [Sides] -> [Sides]
+threeSidesToColumnGroups sides
+  | length sides < 3 = []
+  | otherwise = [map head sides, map (head . tail) sides, map last sides]
--- aoc-day03/test/AocDay03Spec.hs
+++ aoc-day03/test/AocDay03Spec.hs
@@ -15,16 +15,25 @@ spec :: Spec
 spec = do
   describe "validTrangle" $ do
     it "returns True for valid triangle sides" $ do
-      validTriangle (5, 6, 10) `shouldBe` True
-      validTriangle (10, 6, 5) `shouldBe` True
-      validTriangle (3, 2, 3) `shouldBe` True
+      validTriangle [5, 6, 10] `shouldBe` True
+      validTriangle [10, 6, 5] `shouldBe` True
+      validTriangle [3, 2, 3] `shouldBe` True
 
     it "returns False from invalid triangle sides" $ do
-      validTriangle (5, 10, 25) `shouldBe` False
-      validTriangle (10, 5, 30) `shouldBe` False
+      validTriangle [5, 10, 25] `shouldBe` False
+      validTriangle [10, 5, 30] `shouldBe` False
 
   describe "lineToSides" $
     it "splits a line on spaces into a Sides tuple" $ do
-      lineToSides "  34   456  23  " `shouldBe` (34, 456, 23)
-      lineToSides "34 46  23 " `shouldBe` (34, 46, 23)
-      lineToSides " 34     46  23" `shouldBe` (34, 46, 23)
+      lineToSides "  34   456  23  " `shouldBe` [34, 456, 23]
+      lineToSides "34 46  23 " `shouldBe` [34, 46, 23]
+      lineToSides " 34     46  23" `shouldBe` [34, 46, 23]
+
+  describe "sideListToColumnGroups" $ do
+    it "switches row groups to column groups" $ do
+      sideListToColumnGroups [[1,1,1], [2,2,2], [3,3,3]] `shouldBe` [[1,2,3], [1,2,3], [1,2,3]]
+      sideListToColumnGroups [[4,4,4], [2,2,2], [3,3,3]] `shouldBe` [[4,2,3], [4,2,3], [4,2,3]]
+
+    it "ignores trailing groups of < 3" $ do
+      sideListToColumnGroups [[1,1,1], [2,2,2], [3,3,3], [9,9,9]] `shouldBe` [[1,2,3], [1,2,3], [1,2,3]]
+      sideListToColumnGroups [[4,4,4], [2,2,2], [3,3,3], [9,9,9], [0,0,0]] `shouldBe` [[4,2,3], [4,2,3], [4,2,3]]
